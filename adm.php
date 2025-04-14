f3153d6bd2c1ee47859853cbdb0f57f3f7ccdc89
Lucas silva <sceiferlucas@hotmail.com>
2025-04-14 17:55:12 -0300

    Evillyn3D

--- adm.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	include 'header.php';
- ?>
-
- <div class="main center">
- 	<a class="botao-adm" href="opt-torneios.php">TORNEIOS</a>
- 	<a class="botao-adm" href="lista-jogadores.php">JOGADORES</a>
- 	<a class="botao-adm" href="relatorios.php">RELATORIOS</a>
- </div>
--- opt-torneios.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	include 'header.php';
-?>
-
- <div class="main center">
- 	<a class="botao-adm" href="form-cadastrar-torneio.php">CRIAR TORNEIO</a>
- 	<a class="botao-adm" href="lista-torneios.php">LISTAR TORNEIOS</a>
- 	
- </div>
